aeaaa096e8f1d3e973dfe4f06490b72db47f095d
EVEB555 <97383055+EVEB555@users.noreply.github.com>
2023-01-19 08:42:15 +0200

    updated convertDecimal logit to accept negative numbers

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,7 +1,6 @@
 package converter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 //TODO reverse Arraylist
 public class Converter {
@@ -15,16 +14,53 @@ public class Converter {
     public static float fractionOf(float x) {
         return x - (int) x;
     }
-    public Object convertDecimal(){
-        ArrayList<Object> octalValues = new ArrayList<>();
 
-        do{
-            float octalNumber = decimalValue/octalBase;
-            float remainder = fractionOf(octalNumber);
-            Integer convertedOctalValue = Math.round(remainder * 8);
-            octalValues.add(convertedOctalValue);
-            decimalValue = (int) octalNumber;
-            } while (decimalValue > 0);
-        return octalValues;
+    public ArrayList<Integer> reverseArrayList(ArrayList<Integer> list){
+        for(int i = 0; i < list.size()/2; i++){
+            Integer temp = list.get(i);
+            list.set(i, list.get(list.size() - i -1));
+            list.set(list.size() - i - 1, temp);
+        }
+
+        return list;
+    }
+
+    public String printElements(ArrayList<Integer> list){
+        for (Integer integer : list) {
+            System.out.print(integer);
+        }
+        return "";
+    }
+
+
+    public String convertDecimal(){
+        ArrayList<Integer> octalValues = new ArrayList<>();
+
+        if(decimalValue > 0){
+            do{
+                float octalNumber = decimalValue/octalBase;
+                float remainder = fractionOf(octalNumber);
+                Integer convertedOctalValue = Math.round(remainder * 8);
+                octalValues.add(convertedOctalValue);
+                decimalValue = (int) octalNumber;
+            }while(decimalValue > 0);
+
+        } else {
+            do{
+                float octalNumber = decimalValue/octalBase;
+                float remainder = fractionOf(octalNumber);
+                Integer convertedOctalValue = Math.round(remainder * 8);
+                octalValues.add(convertedOctalValue);
+                decimalValue = (int) octalNumber;
+            } while (decimalValue < 0);
+
+            for (int i = 0; i < octalValues.size() - 1; ++i){
+                if (octalValues.get(i) < 0) {
+                    int absoluteValue = Math.abs(octalValues.get(i));
+                    octalValues.set(i, absoluteValue);
+                }
+            }
+        }
+        return printElements(reverseArrayList(octalValues));
     }
 }
