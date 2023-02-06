b39e5ff41f0eb8a4719b003607e94ce394cdc70e
EVEB555 <97383055+EVEB555@users.noreply.github.com>
2023-02-06 13:14:59 +0200

    Updated GUI

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -2,7 +2,7 @@ package converter;
 
 import java.util.ArrayList;
 
-//TODO fix printElements()
+//TODO fix convertDecimal logic
 public class Converter {
     private int decimalValue;
     private float octalBase = 8f;
@@ -15,53 +15,56 @@ public class Converter {
         return x - (int) x;
     }
 
-    public ArrayList<Integer> reverseArrayList(ArrayList<Integer> list){
-        for(int i = 0; i < list.size()/2; i++){
+    public ArrayList<Integer> reverseArrayList(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() / 2; i++) {
             Integer temp = list.get(i);
-            list.set(i, list.get(list.size() - i -1));
+            list.set(i, list.get(list.size() - i - 1));
             list.set(list.size() - i - 1, temp);
         }
 
         return list;
     }
 
-    public String printElements(ArrayList<Integer> list){
+    public String printElements(ArrayList<Integer> list) {
+        StringBuilder result = new StringBuilder();
+
         for (Integer integer : list) {
-            System.out.print(integer);
+            result.append(integer);
         }
-        return ""; //correct this for program to return value in GUI
+
+        return result.toString();
     }
 
-
-    public String convertDecimal(){
+    public String convertDecimal() {
         ArrayList<Integer> octalValues = new ArrayList<>();
 
-        if(decimalValue > 0){
-            do{
-                float octalNumber = decimalValue/octalBase;
+        if (decimalValue > 0) {
+            do {
+                float octalNumber = decimalValue / octalBase;
                 float remainder = fractionOf(octalNumber);
                 Integer convertedOctalValue = Math.round(remainder * 8);
                 octalValues.add(convertedOctalValue);
                 decimalValue = (int) octalNumber;
-            }while(decimalValue > 0);
+            } while (decimalValue > 0);
 
         } else {
-            do{
-                float octalNumber = decimalValue/octalBase;
+            do {
+                float octalNumber = decimalValue / octalBase;
                 float remainder = fractionOf(octalNumber);
                 Integer convertedOctalValue = Math.round(remainder * 8);
                 octalValues.add(convertedOctalValue);
                 decimalValue = (int) octalNumber;
             } while (decimalValue < 0);
 
-            for (int i = 0; i < octalValues.size() - 1; ++i){
+            for (int i = 0; i < octalValues.size() - 1; ++i) {
                 if (octalValues.get(i) < 0) {
                     int absoluteValue = Math.abs(octalValues.get(i));
                     octalValues.set(i, absoluteValue);
                 }
             }
         }
-        return printElements(reverseArrayList(octalValues));
 
+        return printElements(reverseArrayList(octalValues));
     }
+
 }
--- src/converter/Main.java
+++ src/converter/Main.java
@@ -17,19 +17,26 @@ public class Main {
     public static JButton btnCalculateDecToOct;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         // Set up the frame
         frmMain = new JFrame("Decimal to Octal Converter");
-        frmMain.setSize(250, 300);
-        frmMain.setLayout(new FlowLayout()); //automatically places the elements
+        frmMain.setSize(400, 250);
+        frmMain.setLayout(null);
+        //frmMain.setLayout(new GridLayout(3, 2,10,10)); //automatically places the elements
+
+        //declaring panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,2, 10, 10));
+        panel.setSize(100, 100);
+        panel.setBounds(50, 50, 300, 150);
 
         // Create GUI Elements
         lblDecimal = new JLabel("Decimal value:");
         textDecimal = new JTextField(10); //number refers to number of columns
 
         lblOctal = new JLabel("Octal value:");
-        textOctal = new JTextField(10);
+        textOctal = new JTextField(20);
+        textOctal.setEditable(false);
 
         btnCalculateDecToOct = new JButton("Convert");
 
@@ -38,38 +45,31 @@ public class Main {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        // Convert
-                        String decimalInput = textDecimal.getText();
-                        int decimalInputAsInt = Integer.parseInt(decimalInput);
-                        Converter converter = new Converter(decimalInputAsInt);
-                        //System.out.println(converter.convertDecimal());
-                        String convertedOctal = converter.convertDecimal();
-                        textOctal.setText(convertedOctal);
+                        try {
+                            // Convert
+                            String decimalInput = textDecimal.getText();
+                            int decimalInputAsInt = Integer.parseInt(decimalInput);
+                            Converter converter = new Converter(decimalInputAsInt);
+                            String convertedOctal = converter.convertDecimal();
+                            textOctal.setText(convertedOctal);
+                        }catch (NumberFormatException exception){
+                            JOptionPane.showMessageDialog(frmMain, "Invalid input.\n  *Try shorter number\n  *Enter only digits");
+                        }
                     }
                 }
         );
 
-
         // Add the GUI Elements to the frame
-        frmMain.add(lblDecimal);
-        frmMain.add(textDecimal);
-        frmMain.add(lblOctal);
-        frmMain.add(textOctal);
-        frmMain.add(btnCalculateDecToOct);
+        panel.add(lblDecimal);
+        panel.add(textDecimal);
+        panel.add(lblOctal);
+        panel.add(textOctal);
+        panel.add(btnCalculateDecToOct);
+
+        frmMain.add(panel);
 
         // Make the frame visible
         frmMain.setVisible(true);
-
-
-       /* try{
-            System.out.println("Enter decimal value");
-            int input = sc.nextInt();
-
-            Converter converter = new Converter(input);
-            System.out.println(converter.convertDecimal());
-        }catch (InputMismatchException e){
-            System.err.println("Invalid input (try digits/shorter input)");
-        }*/
 
     }
 }
